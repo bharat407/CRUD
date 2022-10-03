0e49cbfb1617c98c9326f01315f4a0bf5aefc3da
Bharat kushwah <85440714+bharat407@users.noreply.github.com>
2022-10-03 19:27:10 +0530

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,7 +1,7 @@
 import pymysql as sql
 try:
  db = sql.connect(host=input("Enter your Host name:--  "), port=int(input("Enter your port no:-- ")), user=input("Enter your user name:--  "), passwd=input("Enter your passwd:--  "),db=input("Enter your database:-- "))
- #db=sql.connect(host="localhost",port=3306,user="root",passwd="123",db="resturant")
+ #db=sql.connect(host="localhost",port=3306,user="root",passwd="xxx",db="resturant")
  cmd=db.cursor()
  id=input("Enter Product Id U Want to Update:")
  q="Select * from Products where Productid='{0}'".format(id)

1bce9829b91898f375d95613543142eb3fc9c0f0
Bharat kushwah <85440714+bharat407@users.noreply.github.com>
2022-10-03 19:29:26 +0530

    Update Joins.py

--- Joins.py
+++ Joins.py
@@ -1,6 +1,6 @@
 import pymysql as sql
 try:
- db=sql.connect(host="localhost",port=3306,user="root",passwd="123",db="appel")
+ db=sql.connect(host="localhost",port=3306,user="root",passwd="xxx",db="appel")
  cmd=db.cursor()
  bn=input("Enter Bill Number:")
  q="Select P.productid,P.productname,P.price,P.offer,S.qtysale,P.offer*S.qtysale as amount from products P,sale S where P.productid=S.productid and S.billno={0}".format(bn)
